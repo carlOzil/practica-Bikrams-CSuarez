aed9c555568b0217330a441642d72373fe8147a0
carlOzil <seifer01es@msn.com>
2023-06-19 04:46:03 +0200

    last one

---
+++ index.js
@@ -0,0 +1,25 @@
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = PI * 2;
+let booleanoAnd = (booleano1 && booleano2);
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+
+
+
+
+
+let variableValorNumerico = 3;
+const MiNombre = 'tu nombre';
+const MiNumeroFav = 7;
+let booleanoOr = (booleano1 || booleano2);
+let booleanoMix1 = (booleano1 && (TAU / 2 === PI));
+let seisNoEsNueve = (6 !== 9);
+let booleanoMix2 = (variableValorNumerico > 0 || x < (MiNumeroFav + TAU));
+let valorSuma = (MiNumeroFav + variableValorNumerico);
+let valorResta = (MiNumeroFav - variableValorNumerico);
+let valorMultiplicacion = (MiNumeroFav * variableValorNumerico);
+let valorDivision = (MiNumeroFav / 3);
+

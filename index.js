13f02a2f41c9b16272e165233e13c9b7e852d7f5
carlOzil <seifer01es@msn.com>
2023-06-19 09:48:52 +0200

    five left

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const TAU = PI * 2;
 let booleanoAnd = (booleano1 && booleano2);
 let booleanoNot = !booleano1;
 let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
-
-
-
-
-
+let incrementarDesp = 2
+let resultadoDesp = incrementarDesp++
+let incrementarAntes = 2
+let resultadoAntes = ++incrementarAntes
+// falta la 10
+let postI = 0
+let postJ = 0
+for (let i = 0; i <= 11; i++) {
+}
+postI += postJ++
+// falta la 12
 let variableValorNumerico = 3;
 const MiNombre = 'tu nombre';
 const MiNumeroFav = 7;
@@ -23,3 +29,6 @@ let valorResta = (MiNumeroFav - variableValorNumerico);
 let valorMultiplicacion = (MiNumeroFav * variableValorNumerico);
 let valorDivision = (MiNumeroFav / 3);
 
+
+
+// faltan tres ultimas
